71a4d1473d89f280d4df9b1751b94237e55b62b7
Gabriel <gabriel.sigurd.cabrera@gmail.com>
2019-07-24 00:09:38 +0200

    Fixing tree

---
+++ Termighty.py
@@ -0,0 +1,6 @@
+from .lib.Char import Char
+from .lib.Charray import Charray
+from .lib.String import String
+from .lib.Terminal import Terminal
+from .utils.utils import *
+from .utils.unit_tests import run_unit_tests
