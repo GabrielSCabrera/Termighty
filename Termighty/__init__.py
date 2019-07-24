d1fa2e88cf2120cebe92d3e0c3d99cc3784e787d
gabrielscabrera <gabriel.sigurd.cabrera@gmail.com>
2019-07-24 15:15:13 +0200

    Changed structure, released first alpha

---
+++ Termighty/__init__.py
@@ -0,0 +1,7 @@
+from .obj.Char import Char
+from .obj.Charray import Charray, delete_Charray, load_Charray, display_logo
+from .obj.Charray import design_Charray, big_letter, big_word, hcat, vcat
+from .obj.String import String
+from .obj.Terminal import Terminal
+from .utils.utils import *
+from .utils.unit_tests import run_unit_tests
---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'Termighty',
+  packages = ['Termighty'],
+  version = '0.0.1',      # Start with a small number and increase it with every change you make
+  # license='GPLv3',
+  description = 'A Python-based GUI development package for Ubuntu\'s Gnome terminal',
+  author = 'Gabriel S. Cabrera',
+  author_email = 'gabric@uio.no',      # Type in your E-Mail
+  url = 'https://github.com/GabrielSCabrera/Termighty',
+  download_url = 'https://github.com/GabrielSCabrera/Termighty/archive/v0.0.1.tar.gz',    # I explain this later on
+  keywords = ['terminal', 'gui', 'interactive'],   # Keywords that define your package best
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',   # Again, pick a license
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
